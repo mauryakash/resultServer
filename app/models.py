9e14b30bc060c227f7514a9fe9f81f7fe07a2455
mauryakash <mauryakash@gmail.com>
2021-10-27 00:49:27 +0530

    added mobile no as string field

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ class Student(db.Model):
     rollno = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(20), unique=False, nullable=False)
     email = db.Column(db.String(30), unique=True, nullable=False)
-    mobile = db.Column(db.Integer(), unique=True, nullable=False)
+    mobile = db.Column(db.String(10), unique=True, nullable=False)
     math_marks = db.Column(db.Integer())
     science_marks = db.Column(db.Integer())
     english_marks = db.Column(db.Integer())
