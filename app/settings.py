c5e1a26724f0d5aa384030d423fbc9391c917ec8
mauryakash <mauryakash@gmail.com>
2021-10-26 13:59:24 +0530

    Last Commit before deployment and added JawsDB URI

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,6 @@
 
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Ram#12345@localhost/result'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Ram#12345@localhost/result'
+SQLALCHEMY_DATABASE_URI = 'mysql+plmysql://hskxc6lfjmrtp8nh:upnvbqonrhadpcj8@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fkwlpple2zut8oa5'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "secretkey1" #admin #Session Otp
