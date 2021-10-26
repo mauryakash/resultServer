9272fa5d9107a1cb179c92690dd51a3e23b3b907
mauryakash <mauryakash@gmail.com>
2021-10-26 18:08:26 +0530

    Database URI Modification

--- app/settings.py
+++ app/settings.py
@@ -4,5 +4,3 @@ import os
 SQLALCHEMY_DATABASE_URI = os.environ.get('JAWSDB_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "secretkey1" #admin #Session Otp
-
-# DB_CONN = "mysql+pymysql://hskxc6lfjmrtp8nh:upnvbqonrhadpcj8@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fkwlpple2zut8oa5"
