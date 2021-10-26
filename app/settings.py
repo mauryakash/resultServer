d97e507e390a30f807a0ae52a723f66bb568f755
mauryakash <mauryakash@gmail.com>
2021-10-26 16:49:21 +0530

    added the new db url and Env File

--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,8 @@
-
+import os
 
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Ram#12345@localhost/result'
-SQLALCHEMY_DATABASE_URI = 'mysql+plmysql://hskxc6lfjmrtp8nh:upnvbqonrhadpcj8@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fkwlpple2zut8oa5'
+SQLALCHEMY_DATABASE_URI = os.environ.get('JAWSDB_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "secretkey1" #admin #Session Otp
+
+# DB_CONN = "mysql+pymysql://hskxc6lfjmrtp8nh:upnvbqonrhadpcj8@nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/fkwlpple2zut8oa5"
